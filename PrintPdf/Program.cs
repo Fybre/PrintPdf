bc4643d02e15c5cff6110b5dd335ef2c1bb710de
Craig <craig.mewett@canon.com.au>
2025-02-21 22:23:40 +1100

    Add project files.

---
+++ PrintPdf/Program.cs
@@ -0,0 +1,33 @@
+﻿using Rmg.PdfPrinting;
+using System.IO;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine(@"Usage: PrintPdf.exe document.pdf printername
+Where:
+    document.pdf        File to print
+    printer name        Printer to use.
+
+Surround parameters with quotes """" if spaces in the names.
+Default printer options will be used when printing.
+
+Uses: https://github.com/mgaffigan/Rmg.PdfPrinting
+License: https://mit-license.org/
+
+Discord: .Fybre.");
+    return -1;
+}
+
+try
+{
+    var fileToPrint = Path.GetFullPath(args[0]);
+    var printerName = args[1];
+    var printer = new PdfPrinter();
+    await printer.Print(printerName, fileToPrint);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Error: {ex}");
+    return -1;
+}
+return 0;
